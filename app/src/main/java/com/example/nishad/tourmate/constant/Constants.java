a46ffe1ddcf610261c63c7c5922f4fd92642c0a2
Mdsun <sun.com420@gmail.com>
2016-10-29 00:23:18 +0600

    My android apps for tourist

---
+++ app/src/main/java/com/example/nishad/tourmate/constant/Constants.java
@@ -0,0 +1,15 @@
+package com.example.nishad.tourmate.constant;
+
+/**
+ * Created by Nishad on 8/21/2016.
+ */
+public class Constants {
+
+    public static final String LOGIN_SIGNUP_ADD_EVENT = "loginOrSignUp";
+    public static final String USER_EMAIL = "userEmail";
+    public static final String USER_ID = "userId";
+    public static final String EVENT_ID = "eventId";
+    public static final String BASE_URL = "http://api.openweathermap.org/";
+    public static final String API_KEY = "e5cd837473d78b88cd21ef4e3e43e5dd";
+
+}
